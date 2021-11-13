f10a68ac10953c1c9b0b4d3c9efbd7f13dafd0de
ekim412 <tkim288@uwo.ca>
2021-11-13 15:43:32 -0500

    layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,13 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
-
 // Step 2: Define your component
-const IndexPage = () => {
+const AboutPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-    <p>I'm making this by following the Gatsby Tutorial.</p>
+    <Layout>
+        <p>Hi I'm Edward</p>
     </Layout>
   )
 }
-
 // Step 3: Export your component
-export default IndexPage
+export default AboutPage
