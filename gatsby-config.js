26affea0c07060e6ffe18df47f37ed74780d26ca
ekim412 <tkim288@uwo.ca>
2021-11-23 19:13:07 -0500

    projects page

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,22 +5,14 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
     "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: `projects`,
-        path: `${__dirname}/projects`,
-      }
+        path: `${__dirname}/projects/`,
+      },
     },
+    "gatsby-plugin-mdx",
   ],
 };
